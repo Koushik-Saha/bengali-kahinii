141b650c326f4adf49f183dcb58038cdfbc808b1
Koushik Saha <koushik.saha666@gmail.com>
2025-02-26 20:57:25 -0800

    add menu header

--- constants/headerNavLinks.ts
+++ constants/headerNavLinks.ts
@@ -1,9 +1,10 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  { href: '/homepage', title: 'HomePage' },
+  { href: '/popular-stories', title: 'Popular Stories' },
+  { href: '/popular-writers', title: 'Popular Writers' },
+  { href: '/submit-a-story', title: 'Submit a Story' },
+  { href: '/indian-live-sex', title: 'Indian Live Sex' },
 ]
 
 export default headerNavLinks
